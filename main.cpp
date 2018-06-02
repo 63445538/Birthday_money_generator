446edfb52543f2b843cad1ea0d39ff7a0c79deb8
caorui <slayerkillme@gmail.com>
2018-06-02 14:10:08 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 { 
    long unsigned int a,b,c,d= 0;
-   a = 0xD0ED;   //许（GB18030)
-   b = 0xE5AB;   //瀚
-   c = 0xE2F9;   //怡
+   a = 0xD0ED;   //（GB18030)
+   b = 0xE5AB;  
+   c = 0xE2F9;   
    d = a + b + c;
    srand(d); 
    cout <<  (rand() % (200))+ 1 << '\t'; 
